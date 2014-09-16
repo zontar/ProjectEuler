768ecd630a206d6bc888143a97ec9f03b855a87b
Vladimir <vladimir@zontar.ru>
2014-09-16 14:35:13 +0700

    Первая задача решена

--- euler/task1.cpp
+++ euler/task1.cpp
@@ -2,8 +2,24 @@
 
 const std::string taskName = "Task1";
 
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+//Сумма чисел кратных k до n
+unsigned int sum(unsigned int k, unsigned int n)
+{
+	if(k>n) return 0;
+	unsigned int count = (n-1)/k;
+	return (count*(count+1)/2)*k;
+}
+
+
 int run()
 {
-	std::cout << "checked in";
+	unsigned int n = 1000;
+	unsigned int result = sum(3, n) + sum(5, n) - sum(15, n);
+	std::cout << result;
 	return 0;
 }

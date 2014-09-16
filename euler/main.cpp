1c130d3dee89b7b18fec692c1f1f749e7ad14a7d
Vladimir <vladimir@zontar.ru>
2014-09-16 14:07:17 +0700

    Слоуфикс

--- euler/main.cpp
+++ euler/main.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-#include <time.h>
+#include <ctime>
 #include "task.h"
 
 extern const std::string taskName;
@@ -12,11 +12,11 @@ int main()
 	std::freopen(fileName.data(), "w", stdout);
 	clock_t bTime = clock();
 	std::cout << "run on " << bTime << std::endl;
-	std::cout << "----------------------------------------------------------------------------" << std::endl;
+	std::cout				<< "----------------------------------------------------------------------------" << std::endl;
 	run();
 	clock_t eTime = clock();
-	std::cout << std::endl << "----------------------------------------------------------------------------" << std::endl;
+	std::cout << std::endl	<< "----------------------------------------------------------------------------" << std::endl;
 	std::cout << "finished on " << eTime << std::endl;
-	std::cout << "time: " << eTime-bTime << std::endl;
+	std::cout << "ticks: " << eTime-bTime << "; time: " << (double)(eTime-bTime)/CLOCKS_PER_SEC <<  std::endl;
 	return 0;
 }
